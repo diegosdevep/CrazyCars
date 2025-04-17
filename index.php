2939389132c5aed38d1de39096858fa1913c190d
Diego Maidana <diegodevep@gmail.com>
2025-04-17 19:47:16 +0200

    checking bootstrap

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Crazy Cars</title>
+</head>
+
+<body>
+    <h1 class="text-center text-primary">Crazy Cars test</h1>
+</body>
+
+</html>
